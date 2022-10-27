6784578b125928707b0c55246a342819fabf1c46
yurididomenico <yurididomenico93@gmail.com>
2022-10-27 14:50:28 +0200

    JS: Funzione per invertire la parola inserita

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,15 +2,30 @@
 
 console.log("YDD");
 
+/*
+Palidroma
+Chiedere all’utente di inserire una parola
+Creare una funzione per capire se la parola inserita è palindroma
+*/
 
 
+let parola = "ciao";
+let parolaMirror = "";
 
 
+function checkPalindro(x)
+{
+    for(let i = parola.length -1; i >= 0; i--)
+    {   
+        parolaMirror += parola[i];
+    }
+}
+
+checkPalindro(parola);
 
 
-
-
-
+console.log(parola)
+console.log(parolaMirror)
 
 
 
